263fe861310f48e0d9cfb8af7df5cb0a2e81dc67
Nick Dumas <dumasn@homes.com>
2019-10-04 13:21:47 -0400

    Add godoc strings.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// New initializes a new Counter and its backing map.
 func New() Counter {
 	var c Counter
 	c.members = make(map[string]int)
@@ -9,14 +10,18 @@ func New() Counter {
 	return c
 }
 
+// Counter uses a map[<type>]int to count how many instances of a value have been observed.
+// Inspired by Python's collections.Counter type.
 type Counter struct {
 	members map[string]int
 }
 
+// Add accepts a value and increments its observation count by 1.
 func (c *Counter) Add(m string) {
 	c.members[m]++
 }
 
+// Get returns the number of times a value has been observed.
 func (c *Counter) Get(m string) int {
 	n, ok := c.members[m]
 	if !ok {
@@ -26,12 +31,16 @@ func (c *Counter) Get(m string) int {
 	return n
 }
 
+// Most returns the n most frequently observed values, as a Counter.
+// This is to faciliate further filtering of the provided data.
 func (c *Counter) Most(n int) Counter {
 	var r Counter
 
 	return r
 }
 
+// Least returns the n least frequently observed values, as a Counter.
+// This is to faciliate further filtering of the provided data.
 func (c *Counter) Least(n int) Counter {
 	var r Counter
 
