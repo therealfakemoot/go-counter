65fa1ceb493e71d98214ef4988f363e18b9b098a
Nick Dumas <dumasn@homes.com>
2019-10-04 13:22:00 -0400

    Restructuring tests.
    A little static analysis indicates that there are no failure scenarios
    for adding values to a map: it will always succeed.
    
    Factored the Counter creation code out into a helper function to keep
    things DRY.
    
    Still hammering out naming for the tests and junk.

--- counter_test.go
+++ counter_test.go
@@ -4,21 +4,40 @@ import (
 	"testing"
 )
 
-func Test_CounterAdd(t *testing.T) {
+func prepareTestCounter() Counter {
+
 	c := New()
 	c.Add("foo")
 	c.Add("foo")
 	c.Add("bar")
+	return c
+}
 
-	n := c.Get("foo")
-	if n != 2 {
-		t.Logf(`expected 2 "foo", got %d`, n)
-		t.Fail()
-	}
+func Test_CounterBasic(t *testing.T) {
+	c := prepareTestCounter()
 
-	n = c.Get("bar")
-	if n != 1 {
-		t.Logf(`expected 1 "bar", got %d`, n)
-		t.Fail()
-	}
+	t.Run("get observations", func(t *testing.T) {
+		n := c.Get("foo")
+		if n != 2 {
+			t.Logf(`expected 2 "foo", got %d`, n)
+			t.Fail()
+		}
+
+		n = c.Get("bar")
+		if n != 1 {
+			t.Logf(`expected 1 "bar", got %d`, n)
+			t.Fail()
+		}
+	})
+}
+
+func Test_CounterFilter(t *testing.T) {
+	c := prepareTestCounter()
+	t.Run("most", func(t *testing.T) {
+
+	})
+
+	t.Run("least", func(t *testing.T) {
+
+	})
 }
